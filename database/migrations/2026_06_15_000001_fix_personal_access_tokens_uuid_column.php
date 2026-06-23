9e4a6cf403e1fa082954f15f456ff191799643f8
Abubakar Ahmadu <biggulfgroup@gmail.com>
2026-06-24 06:53:29 +0800

    fix: replace MySQL MODIFY with PostgreSQL ALTER COLUMN TYPE syntax

--- database/migrations/2026_06_15_000001_fix_personal_access_tokens_uuid_column.php
+++ database/migrations/2026_06_15_000001_fix_personal_access_tokens_uuid_column.php
@@ -7,11 +7,11 @@ return new class extends Migration
 {
     public function up(): void
     {
-        DB::statement('ALTER TABLE personal_access_tokens MODIFY tokenable_id CHAR(36) NOT NULL');
+        DB::statement('ALTER TABLE personal_access_tokens ALTER COLUMN tokenable_id TYPE CHAR(36)');
     }
 
     public function down(): void
     {
-        DB::statement('ALTER TABLE personal_access_tokens MODIFY tokenable_id BIGINT UNSIGNED NOT NULL');
+        DB::statement('ALTER TABLE personal_access_tokens ALTER COLUMN tokenable_id TYPE BIGINT USING tokenable_id::BIGINT');
     }
 };
